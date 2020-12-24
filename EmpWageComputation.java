95c1aa4a1e3ca5a219e70a649f584da7a4c00527
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-24 23:42:30 +0530

    Add Employee Wage Computation UC1 Problem to UC1 branch

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -3,5 +3,19 @@ public class EmpWageComputation
 	public static void main(String args [])
 	{
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+		 int Present=1;
+
+                int empCheck=(int)Math.floor(Math.random()*10)%2;
+                System.out.println("Random value "+empCheck);
+
+                if(empCheck==Present)
+                {
+                        System.out.println("Employpee is Presnt");
+                }
+                else
+                {
+                        System.out.println("Employee is Absent");
+                }
+
 	}
 }
