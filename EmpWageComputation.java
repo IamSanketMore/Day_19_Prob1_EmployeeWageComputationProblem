612160b0af0d58625820f084cee073cea81ba9f6
IamSanketMore <sanketmore.999.m@gmail.com>
2020-12-25 00:12:36 +0530

    Add Employee Wage Computation UC5 Problem to UC5 branch

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -10,32 +10,41 @@ public class EmpWageComputation
 		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 		System.out.println("--------------------------------------------------------------------");
 
-		int Emp_Hrs=0;
+                int Emp_Hrs=0;
                 int Emp_Rate_Per_Hrs=20;
                 int Per_Day_Salary=0;
-
+                int Num_Of_Working_Days=20;
+                int Per_Month_Salary=0;
                 int Result=(int)Math.floor(Math.random()*10)%3;
                 System.out.println("Result is :-"+Result);
 
-                switch ((int)Result)
+                for( int day=1; day<=Num_Of_Working_Days; day++ )
                 {
-                        case Full_Time :
-                        System.out.println("Employee is Presnt and Its Full Time Employee");
-                        Emp_Hrs=8;
-                        break;
+                        switch ((int)Result)
+                        {
+                               case Full_Time :
+                                       //System.out.println("Employee is Presnt and Its Full Time Employee");
+                                        Emp_Hrs=8;
+                                break;
 
-                        case Part_Time :
-                        System.out.println("Employee is Presnt and Its Part Time Employee");
-                        Emp_Hrs=4;
-                        break;
+                                case Part_Time :
+                                        //System.out.println("Employee is Presnt and Its Part Time Employee");
+                                        Emp_Hrs=4;
+                                break;
 
-                        default :
-                        System.out.println("Employee is Absent");
-                        Emp_Hrs=0;
-                        Per_Day_Salary=0;
+                                default :
+                                        //System.out.println("Employee is Absent");
+                                        Emp_Hrs=0;
+                                        Per_Day_Salary=0;
+                        }
+
+                        Per_Day_Salary = (Emp_Hrs*Emp_Rate_Per_Hrs);
+                        Per_Month_Salary=( Per_Month_Salary+Per_Day_Salary);
+
                 }
+                        System.out.println("Per Day Salary is :-"+Per_Day_Salary);
+                        System.out.println("Per Month Salary is :-"+Per_Month_Salary);
 
-                Per_Day_Salary = (Emp_Hrs*Emp_Rate_Per_Hrs);
-                System.out.println("Per Day Salary is :-"+Per_Day_Salary);
         }
+
 }
